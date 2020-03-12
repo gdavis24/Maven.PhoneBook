fcb65dc2dd68c4cbbbcb3488a7a881c14bcf604f
Greg Davis <jrobley3@hotmail.com>
2020-03-11 22:44:01 -0400

    making progress on lab

--- src/main/java/com/zipcodewilmington/phonebook/PhoneBook.java
+++ src/main/java/com/zipcodewilmington/phonebook/PhoneBook.java
@@ -1,5 +1,7 @@
 package com.zipcodewilmington.phonebook;
 
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -7,20 +9,28 @@ import java.util.Map;
  * Created by leon on 1/23/18.
  */
 public class PhoneBook {
+    private Map<String, List<String>> map;
     public PhoneBook(Map<String, List<String>> map) {
+        this.map = map;
     }
 
     public PhoneBook() {
-        this(null);
+        this.map = new LinkedHashMap<>();
     }
 
     public void add(String name, String phoneNumber) {
+        List<String> addNum = new ArrayList<>();
+        addNum.add(phoneNumber);
+        map.put(name, addNum);
     }
 
     public void addAll(String name, String... phoneNumbers) {
+        List<String> addAllNum = new ArrayList<>();
+
     }
 
     public void remove(String name) {
+        map.remove(name);
     }
 
     public Boolean hasEntry(String name) {
@@ -28,7 +38,7 @@ public class PhoneBook {
     }
 
     public List<String> lookup(String name) {
-        return null;
+        return map.get(name);
     }
 
     public String reverseLookup(String phoneNumber)  {
@@ -40,6 +50,6 @@ public class PhoneBook {
     }
 
     public Map<String, List<String>> getMap() {
-        return null;
+        return this.map;
     }
 }
